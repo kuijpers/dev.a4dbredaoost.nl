fa514ab69162e3a53e5453b0ce2529c13515fd74
kuijpers <darkknightworks@gmail.com>
2018-09-24 09:56:43 +0200

    Multi-auth part 1
    Created groups Users, Volunteers and Boardmembers. Including migrations
    and seeds

--- database/migrations/2018_09_23_212425_create_boardmembers_table.php
+++ database/migrations/2018_09_23_212425_create_boardmembers_table.php
@@ -18,6 +18,11 @@ class CreateBoardmembersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('city')->nullable();
+            $table->string('phone')->nullable();
+            $table->date('dob')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
