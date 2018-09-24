c6a5335e156c6af86164678cba868ac7a1ce04a0
kuijpers <darkknightworks@gmail.com>
2018-09-24 23:34:25 +0200

    Solved Namespace issue

--- app/Http/Controllers/Main/HomeController.php
+++ app/Http/Controllers/Main/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Main;
+
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/Volunteer/HomeController.php
+++ app/Http/Controllers/Volunteer/HomeController.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Volunteer;
+
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
