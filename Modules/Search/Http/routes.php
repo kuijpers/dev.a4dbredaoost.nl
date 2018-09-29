3b6c2f0a631200d2312dfccbf7ef295d45ee4a7b
kuijpers <darkknightworks@gmail.com>
2018-09-30 00:03:28 +0200

    Created search page and changed route

--- Modules/Search/Http/routes.php
+++ Modules/Search/Http/routes.php
@@ -2,5 +2,5 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'zoeken', 'namespace' => 'Modules\Search\Http\Controllers'], function()
 {
-    Route::get('/', 'SearchController@index')->name('main.search.index');
+    Route::post('/', 'SearchController@index')->name('main.search.index');
 });
--- Modules/Search/Resources/views/index.blade.php
+++ Modules/Search/Resources/views/index.blade.php
@@ -1,9 +1,32 @@
-@extends('search::layouts.master')
+@extends('layouts.main.main')
+
+@section('title')
+    Zoek het op!
+@endsection
+
+@section('css')
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('modules/search/css/main.css') }}" rel="stylesheet">
+@endsection
+
+@section('jstop')
+
+@endsection
+
+@section('jsbottom')
+
+@endsection
+
 
 @section('content')
-    <h1>Hello World</h1>
 
-    <p>
-        This view is loaded from module: {!! config('search.name') !!}
-    </p>
-@stop
+    {{-- Content here--}}
+    <div class="section-a row">
+        <div class="col-md-12">
+            Dit is een lege pagina voor zoeken
+        </div>
+    </div>
+    {{-- End content--}}
+
+@endsection
+
