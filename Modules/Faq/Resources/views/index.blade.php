edff75118e23b4e80e98e42663f81ba9767b49e7
kuijpers <darkknightworks@gmail.com>
2018-09-29 23:57:23 +0200

    Created faq page

--- Modules/Faq/Resources/views/index.blade.php
+++ Modules/Faq/Resources/views/index.blade.php
@@ -1,9 +1,32 @@
-@extends('faq::layouts.master')
+@extends('layouts.main.main')
+
+@section('title')
+     Veelgestelde vragen
+@endsection
+
+@section('css')
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('modules/faq/css/main.css') }}" rel="stylesheet">
+@endsection
+
+@section('jstop')
+
+@endsection
+
+@section('jsbottom')
+
+@endsection
+
 
 @section('content')
-    <h1>Hello World</h1>
 
-    <p>
-        This view is loaded from module: {!! config('faq.name') !!}
-    </p>
-@stop
+    {{-- Content here--}}
+    <div class="section-a row">
+        <div class="col-md-12">
+            Dit is een lege pagina voor de veelgestelde vragen.
+        </div>
+    </div>
+    {{-- End content--}}
+
+@endsection
+
