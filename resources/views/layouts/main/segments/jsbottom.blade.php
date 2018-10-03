03d8401706fecd9e7409908fc6085512d9765072
kuijpers <darkknightworks@gmail.com>
2018-10-03 10:21:27 +0200

    Changes made for alert boxes

--- resources/views/layouts/main/segments/jsbottom.blade.php
+++ resources/views/layouts/main/segments/jsbottom.blade.php
@@ -5,6 +5,14 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-<!-- ToasterJS -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js" integrity="sha256-3blsJd4Hli/7wCQ+bmgXfOdK7p/ZUMtPXY08jmxSSgk=" crossorigin="anonymous"></script>
+
+<script>
+    $(document).ready(function() {
+        // show the alert
+        setTimeout(function() {
+            $(".tosti").alert('close');
+        }, 2000);
+
+    });
+</script>
