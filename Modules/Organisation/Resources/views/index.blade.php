148a8b2fecb778fdc4e866c19c1589000afc68d4
kuijpers <darkknightworks@gmail.com>
2018-10-16 00:05:06 +0200

    Layout built for Organisation page

--- Modules/Organisation/Resources/views/index.blade.php
+++ Modules/Organisation/Resources/views/index.blade.php
@@ -22,12 +22,23 @@
 
     {{-- Content here--}}
     <div class="section-a section-a row">
-        <div class="col-md-12">
-            Dit is een lege pagina voor de organisatie.
-            <br>
-            Hier kan informatie komen te staan over, bijvoorbeeld, het ontstaan van de organistie.
-            <br>
-            Ook kan hier de toekomst visie van de organisatie uitgelegd worden.
+
+        <div class="col-md-8 offset-md-2 border-bottom">
+            <h1>De organisatie</h1>
+        </div>
+
+        <div class="col-md-8 offset-md-2 my-5">
+            Op deze pagina zou het mooi zijn wanneer er een stukje geschreven staat over de organisatie.
+            Te denken valt aan:
+            <ul>
+                <li>Geschiedenis</li>
+                <li>Hoe is het nu</li>
+                <li>Hoe zien we de toekomst</li>
+                <li>Voor wie doen we dit</li>
+                <li>Met wie doen we dit</li>
+            </ul>
+
+            Dit is misschien ook een mooi plaats om iets extra's over de vrijwilligers te schrijven.
         </div>
     </div>
     {{-- End content--}}
