536604c0eb6a16099d776fec608968224c5b5efa
kuijpers <darkknightworks@gmail.com>
2018-10-17 22:40:53 +0200

    Starting the main board page

--- resources/views/board/home.blade.php
+++ resources/views/board/home.blade.php
@@ -1,25 +1,41 @@
-@extends('layouts.board.app')
+@extends('layouts.board.main')
+
+@section('title')
+    Blank page
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('jstop')
+
+@endsection
+
+@section('jsbottom')
+
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Board Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+    <div class="container-fluid half-padding">
+        <div class="template template__blank">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-danger">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Blank Page</h3>
                         </div>
-                    @endif
+                        <div class="panel-body">
+                            <p>This is a blank page.</p>
+                            @component('components.whoslogged')
+                            @endcomponent
 
-                        @component('components.whoslogged')
-                        @endcomponent
-
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
