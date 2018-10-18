b5fed03ef556a885ef04d26f114564ba6c7d4e30
kuijpers <darkknightworks@gmail.com>
2018-10-18 11:07:40 +0200

    Added logout functionality

--- resources/views/board/segments/navtopright.blade.php
+++ resources/views/board/segments/navtopright.blade.php
@@ -4,10 +4,17 @@
             <i class="fa fa-user-circle"></i>
         </a>
         <ul class="dropdown-menu dropdown-menu-right">
-            <div class="sidebar__utitle">Hier auth name</div>
+            <div class="sidebar__utitle">{{ Auth::user()->name }}</div>
             <li><a href="#"><i class="fa fa-user-circle-o"></i>&nbsp; Profile</a></li>
             <li><a href="#"><i class="fa fa-cogs"></i>&nbsp; Settings</a></li>
-            <li><a href="#"><i class="fa fa-power-off"></i>&nbsp; Log Out</a></li>
+            <li>
+                <a href="{{ route('volunteer.logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    <i class="fa fa-power-off"></i>&nbsp; Log Out
+                </a>
+                <form id="logout-form" action="{{ route('board.logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </li>
         </ul>
     </li>
 </ul>
