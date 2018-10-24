78bec7c8a20bd5566145bfc0672eea4fb520f654
kuijpers <darkknightworks@gmail.com>
2018-10-24 22:53:54 +0200

    Added a method so the settings for signed in user can be fetched

--- app/Models/BoardmembersDashboardSettings.php
+++ app/Models/BoardmembersDashboardSettings.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Auth;
+
 class BoardmembersDashboardSettings extends Model
 {
-    //
+
+	public static function settings()
+	{
+		return static::find(Auth::user()->id);
+	}
+
 }
