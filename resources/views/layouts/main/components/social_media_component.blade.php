842175cb5ec714ee9e5fd19a0da55e679a21d70f
kuijpers <darkknightworks@gmail.com>
2018-10-26 22:26:04 +0200

    Cosmetic changes for the social media component

--- resources/views/layouts/main/components/social_media_component.blade.php
+++ resources/views/layouts/main/components/social_media_component.blade.php
@@ -2,33 +2,16 @@
     <div class="social-media col-md-8 offset-md-2">
         <div class="card bg-success">
             <p class="card-text">
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-twitter-square"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-facebook-messenger"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-google-plus-square"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-slack"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-whatsapp-square"></i>
-                </a>
-                <a href="/#" class="text-white card-link">
-                    <i class="fab fa-youtube-square"></i>
-                </a>
+                <a href="/#" class="text-a4dbo card-link"><i class="fab fa-facebook"></i></a>
+
+                <a href="/#" class="text-a4dbo card-link"><i class="fab fa-twitter-square"></i></a>
+
+                <a href="/#" class="text-a4dbo card-link"><i class="fab fa-instagram"></i></a>
+
+                <a href="/#" class="text-a4dbo card-link"><i class="fab fa-whatsapp-square"></i></a>
+
+                <a href="/#" class="text-a4dbo card-link"><i class="fab fa-youtube-square"></i></a>
+
             </p>
         </div>
     </div>
