631580661c17b6c4519c15e6c728a61896dcae40
kuijpers <darkknightworks@gmail.com>
2018-10-26 22:28:13 +0200

    Overhauled the index layout

--- Modules/Main/Resources/views/index.blade.php
+++ Modules/Main/Resources/views/index.blade.php
@@ -50,13 +50,68 @@
         </div>
     </div>
 
+    <div class="section-c row info-block bg-success pt-5">
+
+        <div class="col-md-12">
+            <div class="card-deck">
+
+                <div class="card">
+                    <h4 class="card-header bg-success text-white">Laatste nieuws</h4>
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Nieuwe mensen in de organisatie.</h5>
+                        <p class="card-text text-a4dbo">
+                            Wat gaat er nu gebeuren met de vernieuwing.
+                            <br>
+                            Sinds kort zitten er aan aantal nieuwe mensen in het bestuur van de organisatie.
+                            <br>
+                            Wat gaat dit betekenen voor de toekomst van de avondvierdaagse in Breda-oost?
+                        </p>
+                    </div>
+                    <div class="card-footer bg-white border-0">
+                        <a href="#" class="btn btn-success">Ga naar dit nieuwsbericht</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <h4 class="card-header bg-success text-white">Wat gaat er gebeuren.</h4>
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Bestuursvergadering</h5>
+                        <p class="text-muted text-success"><small>17-10-2018</small></p>
+                        <p class="card-text text-a4dbo">
+                            Welke speerpunten gaan besproken worden tijdens deze vergadering.
+                        </p>
+                    </div>
+                    <div class="card-footer bg-white border-0">
+                        <a href="#" class="btn btn-success">Meer datums</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <h4 class="card-header bg-success text-white">Wist u dat?</h4>
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Vrijwilligers</h5>
+                        <p class="card-text text-a4dbo">De organisatie draait eigenlijk alleen maar op vrijwilligers.</p>
+                    </div>
+                    <div class="card-footer bg-white border-0">
+                        <a href="#" class="btn btn-success">Meer wist u datjes.</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
     @component('layouts.main.components.social_media_component')
     @endcomponent
 
-    <div class="section-c row info-block bg-success">
-        <div class="col-md-4 offset-md-2">
-            <h4 class="text-a4dbo">Vrijwilligers</h4>
-            <p class="text-white">
+
+    <div class="section-d row">
+
+        <div class="col-md-4">
+            <h4 class="text-success">Vrijwilligers</h4>
+            <p class="text-a4dbo">
                 Hier komt een bedankwoordje naar de vrijwilligers.
                 <br>
                 <br>
@@ -76,8 +131,8 @@
         </div>
 
         <div class="col-md-4">
-            <h4 class="text-a4dbo">Deelnemers</h4>
-            <p class="text-white">
+            <h4 class="text-success">Deelnemers</h4>
+            <p class="text-a4dbo">
                 Hier komt een bedankwoordje naar de deelnemers.
                 <br>
                 <br>
@@ -90,48 +145,23 @@
                 Non igitur bene.
             </p>
         </div>
-    </div>
-
-
-
-    <div class="section-d row">
-        <div class="col-md-4">
-            <div class="card">
-                <h5 class="card-header bg-success text-white">Laatste nieuws</h5>
-                <div class="card-body">
-                    <h5 class="card-title text-a4dbo">Nieuwe mensen in de organisatie.</h5>
-                    <p class="card-text text-a4dbo">
-                        Wat gaat er nu gebeuren met de vernieuwing.
-                    </p>
-                    <a href="#" class="btn btn-light">Ga naar dit nieuwsbericht</a>
-                </div>
-            </div>
-        </div>
 
         <div class="col-md-4">
-            <div class="card">
-                <h5 class="card-header bg-success text-white">Wat gaat er gebeuren.</h5>
-                <div class="card-body">
-                    <h5 class="card-title text-a4dbo">Bestuursvergadering</h5>
-                    <p class="text-muted text-a4dbo"><small>17-10-2018</small></p>
-                    <p class="card-text text-a4dbo">
-                        Welke speerpunten gaan besproken worden tijdens deze vergadering.
-                    </p>
-                    <a href="#" class="btn btn-light">Meer datums</a>
-                </div>
-            </div>
+            <h4 class="text-success">Sponsors</h4>
+            <p class="text-a4dbo">
+                Hier komt een bedankwoordje naar de sponsors.
+                <br>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Et non ex maxima parte de tota iudicabis?
+                Itaque ab his ordiamur.
+                A mene tu?
+                Duo Reges: constructio interrete.
+                Bonum incolumis acies: misera caecitas.
+                Non igitur bene.
+            </p>
         </div>
 
-        <div class="col-md-4">
-            <div class="card">
-                <h5 class="card-header bg-success text-white">Wist u dat?</h5>
-                <div class="card-body">
-                    <h5 class="card-title text-a4dbo">Vrijwilligers</h5>
-                    <p class="card-text text-a4dbo">De organisatie draait eigenlijk alleen maar op vrijwilligers.</p>
-                    <a href="#" class="btn btn-light">Meer wist u datjes.</a>
-                </div>
-            </div>
-        </div>
     </div>
     {{-- End content--}}
 
