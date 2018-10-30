c2d5e2702b3eb06e7781bc7d25185a1883edf605
kuijpers <darkknightworks@gmail.com>
2018-10-30 23:30:30 +0100

    Added new method to check for settings and added var's for mass assignment

--- app/Models/BoardmembersDashboardSettings.php
+++ app/Models/BoardmembersDashboardSettings.php
@@ -9,9 +9,29 @@ use Auth;
 class BoardmembersDashboardSettings extends Model
 {
 
+	protected $fillable = [
+		'boardmembers_id',
+		'boardmembers_theme',
+		'boardmembers_language',
+	];
+
+	// Get the settings for the user
 	public static function settings()
 	{
 		return static::find(Auth::user()->id);
+	}
+
+	// Check if user has settings
+	public static function settings_are_set(){
+
+		if(!empty(static::settings()))
+		{
+
+			return TRUE;
+
+		}
+
+		return FALSE;
 	}
 
 }
