d0c6cbf9ab36be27982e95641037f0254aaefb76
kuijpers <darkknightworks@gmail.com>
2018-10-30 17:17:47 +0100

    Set CSRF token

--- resources/views/board/segments/header.blade.php
+++ resources/views/board/segments/header.blade.php
@@ -2,6 +2,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- provide the csrf token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
     {{--<link rel="icon" type="image/png" href="img/favicon.png">--}}
     {{--<link rel="apple-touch-icon-precomposed" href="img/apple-touch-favicon.png">--}}
 
