072a4b974ab57f8b5c903b7d3e7b5d34fdc175b4
kuijpers <darkknightworks@gmail.com>
2018-11-02 11:33:24 +0100

    Added new route for language select

--- routes/web.php
+++ routes/web.php
@@ -85,4 +85,6 @@ Route::prefix('api')->group(function(){
 
 	Route::post('/board/member/settings/theme-select', 'Board\MemberSettingsController@post_theme');
 
+	Route::post('/board/member/settings/language-select', 'Board\MemberSettingsController@post_language');
+
 });
