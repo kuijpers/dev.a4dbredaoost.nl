236b543a859b3a02b556c61f4d77dde0882126ee
kuijpers <darkknightworks@gmail.com>
2018-11-02 14:14:38 +0100

    Adjusted the routes file

--- Modules/Didyouknow/Http/routes.php
+++ Modules/Didyouknow/Http/routes.php
@@ -1,6 +1,33 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'weetjes', 'namespace' => 'Modules\Didyouknow\Http\Controllers'], function()
+	#####################################
+	#									#
+	#			Main website			#
+	#									#
+	#####################################
+
+
+Route::group(['middleware' => 'web', 'prefix' => 'weetjes', 'namespace' => 'Modules\Didyouknow\Http\Controllers\Main'], function()
 {
     Route::get('/', 'DidyouknowController@index')->name('main.didyouknow.index');
+
+
+
+});
+
+
+	#####################################
+	#									#
+	#			Board website			#
+	#									#
+	#####################################
+
+
+Route::group(['middleware' => 'web', 'prefix' => 'bestuur/weetjes', 'namespace' => 'Modules\Didyouknow\Http\Controllers\Board'], function()
+{
+
+	Route::get('/', 'DidyouknowController@index')->name('board.didyouknow.index');
+
+
+
 });
