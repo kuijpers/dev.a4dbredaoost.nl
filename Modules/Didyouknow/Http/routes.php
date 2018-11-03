f8aa6bde17f0139c413b93efc97981b509161815
kuijpers <darkknightworks@gmail.com>
2018-11-03 22:03:08 +0100

    Built up basic routes for Didyouknow

--- Modules/Didyouknow/Http/routes.php
+++ Modules/Didyouknow/Http/routes.php
@@ -29,5 +29,20 @@ Route::group(['middleware' => 'web', 'prefix' => 'bestuur/weetjes', 'namespace' 
 	Route::get('/', 'DidyouknowController@index')->name('board.didyouknow.index');
 
 
+	Route::get('/create', 'DidyouknowController@create')->name('board.didyouknow.create');
+
+	Route::post('/', 'DidyouknowController@store')->name('board.didyouknow.store');
+
+
+	Route::get('/{id}/edit', 'DidyouknowController@edit')->name('board.didyouknow.edit');
+
+	Route::get('/{id}', 'DidyouknowController@show')->name('board.didyouknow.show');
+
+	Route::patch('/{id}', 'DidyouknowController@update')->name('board.didyouknow.update');
+
+
+	Route::delete('/{id}', 'DidyouknowController@delete')->name('board.didyouknow.delete');
+
+
 
 });
