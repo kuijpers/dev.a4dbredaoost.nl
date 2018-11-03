a9d9ac1d53e47153123793a248cd2deb45c7d727
kuijpers <darkknightworks@gmail.com>
2018-11-03 22:05:24 +0100

    Updated the Main controller so it can get the info from DB

--- Modules/Didyouknow/Http/Controllers/Main/DidyouknowController.php
+++ Modules/Didyouknow/Http/Controllers/Main/DidyouknowController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
+use Modules\Didyouknow\Entities\Models\Main\Didyouknow;
+use Modules\Didyouknow\Entities\Models\Main\Didyouknow_information;
+
+use Carbon\Carbon;
+
 class DidyouknowController extends Controller
 {
 
@@ -15,7 +20,44 @@ class DidyouknowController extends Controller
      */
     public function index()
     {
-        return view('didyouknow::main.index');
+
+    	// Top information
+
+		$didyouknow_information = Didyouknow_information::where('draft', '=', 1)
+			->where('author_approve', '=', 1)
+			->where('editor_approve', '=', 1)
+			->where('publisher_approve', '=', 1)
+			->where('publish_date_start', '<=', Carbon::now())
+			->whereRaw('publish_date_start >= publish_date_end')
+			->where('archived', '=', 0)
+			->get();
+
+
+
+    	// Statements
+
+    	// Temporary data
+		$didyouknows = Didyouknow::where('draft', '=', 1)
+				->where('author_approve', '=', 1)
+				->where('editor_approve', '=', 1)
+				->where('publisher_approve', '=', 1)
+				->where('publish_date_start', '<=', Carbon::now())
+				->whereRaw('publish_date_start <= publish_date_end')
+				->where('archived', '=', 0)
+				->get();
+		// Constant data
+		$didyouknows_1 = Didyouknow::where('draft', '=', 1)
+			->where('author_approve', '=', 1)
+			->where('editor_approve', '=', 1)
+			->where('publisher_approve', '=', 1)
+			->where('publish_date_start', '<=', Carbon::now())
+			->whereRaw('publish_date_start >= publish_date_end')
+			->where('archived', '=', 0)
+			->get();
+
+		$didyouknows = $didyouknows->merge($didyouknows_1);
+
+        return view('didyouknow::main.index')->with(compact('didyouknows', 'didyouknow_information'));
     }
 
 }
