ebfa267dbdc2fd499c2cb616c1c9b5ae6ae657ff
kuijpers <darkknightworks@gmail.com>
2018-11-07 21:26:03 +0100

    Replaced template text with required requests

--- Modules/Didyouknow/Resources/views/board/information/tabs/editor.blade.php
+++ Modules/Didyouknow/Resources/views/board/information/tabs/editor.blade.php
@@ -1,3 +1,114 @@
+@include('didyouknow::board.information.modals.view_author_approved')
+@include('didyouknow::board.information.modals.edit_author_approved')
+
 <div class="tab-pane" id="editor" role="tabpanel">
-    <p>Suspendisse malesuada neque vitae justo auctor imperdiet. Quisque eleifend luctus neque a blandit. Nulla id nunc a est varius molestie sit amet ac mi. Proin tortor est, hendrerit sit amet auctor non, venenatis a metus. Curabitur sit amet lobortis nisl. Sed eget felis sed mi consequat vehicula sit amet sed urna.</p>
+
+    @if(isset($author_approved))
+        {{--{{dd( $author_approved )}}--}}
+        @if($author_approved->isNotEmpty())
+            <div class="template__table_static template__table_responsive">
+                <div class="scrollable scrollbar-macosx">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>
+                                Titel
+                            </th>
+                            <th>
+                                Auteur
+                            </th>
+                            <th>
+                                Goedgekeurd
+                            </th>
+                            <th>
+                                Editor
+                            </th>
+                            <th>
+                                Datum aangemaakt
+                            </th>
+                            <th>
+                                Laatste update
+                            </th>
+                            <th>
+                                &nbsp;
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($author_approved as $author_approve)
+
+                            <tr>
+                                <td>
+                                    {{$author_approve->title}}
+                                </td>
+                                <td>
+                                    {{$author_approve->getAuthorName($author_approve->author_group)}}
+                                </td>
+                                <td class="text-center">
+                                    <div class="checkbox checkbox-success">
+                                        <input id="ch2" type="checkbox" checked="checked" disabled>
+                                        <label for="ch2"></label>
+                                    </div>
+                                </td>
+                                <td>
+                                    @if(!empty($author_approve->editor))
+                                        {{$author_approve->getEditorName($author_approve->editor_group)}}
+                                    @else
+
+                                    @endif
+                                </td>
+                                <td>
+                                    {{$author_approve->created_at->format('d-m-Y H:m:i')}}
+                                </td>
+                                <td>
+                                    {{$author_approve->updated_at->format('d-m-Y H:m:i')}}
+                                </td>
+                                <td>
+                                    <div class="btn-group btn-group-xs" role="group">
+                                        {{-- View the data in Modal--}}
+                                        <button type="button" class="btn btn-info"
+                                                data-toggle="modal"
+                                                data-target="#view_author_approved"
+                                                data-title="{{$author_approve->title}}"
+                                                data-body="{{$author_approve->body}}"
+                                                data-author="{{$author_approve->getAuthorName($author_approve->author_group)}}"
+                                        >
+                                            <i class="fa fa-eye" aria-hidden="true"></i>
+                                            Bekijk
+                                        </button>
+                                        {{-- Edit the data in Modal --}}
+                                        <button type="button" class="btn btn-warning"
+                                                data-toggle="modal"
+                                                data-target="#edit_author_approved"
+                                                data-title="{{$author_approve->title}}"
+                                                data-body="{{$author_approve->body}}"
+                                        >
+                                            <i class="fa fa-eye" aria-hidden="true"></i>
+                                            Wijzig
+                                        </button>
+                                        {{-- Delete/archive the data thru Modal--}}
+                                        <button type="button" class="btn btn-danger"
+                                                data-toggle="modal"
+                                                data-target="#delete_article"
+                                                data-title="{{$author_approve->title}}"
+                                        >
+                                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                            Verwijder
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @else
+            <div class="alert alert-info text-center" role="alert">
+                <i class="alert-ico fa fa-fw fa-exclamation"></i>Op dit moment is er geen data beschikbaar.
+            </div>
+        @endif
+    @endif
+
 </div>
