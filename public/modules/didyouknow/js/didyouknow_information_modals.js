3962cd4d30c277955e83baabbfaa83f34cf42af3
kuijpers <darkknightworks@gmail.com>
2018-11-16 23:11:04 +0100

    Added summernote for new_article id

--- public/modules/didyouknow/js/didyouknow_information_modals.js
+++ public/modules/didyouknow/js/didyouknow_information_modals.js
@@ -1,4 +1,31 @@
 
+	// NEW article
+
+	$('#new_article').summernote({
+		height: 200,                 // set editor height
+		minHeight: null,             // set minimum height of editor
+		maxHeight: null,             // set maximum height of editor
+		focus: true,                 // set focus to editable area after initializing summernote
+		placeholder: 'Write text here.......',
+
+		codemirror: {
+			theme: 'hopscotch',
+			mode: 'htmlmixed',
+			lineWrapping: true,
+			scrollbarStyle: "simple"
+		},
+
+		toolbar: [
+			// [groupName, [list of button]]
+			['style', ['bold', 'italic', 'underline', 'clear']],
+			['font', ['strikethrough', 'superscript', 'subscript']],
+			['fontsize', ['fontsize']],
+			['color', ['color']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['height', ['height']]
+		]
+	});
+
 	// VIEW files
 
 	$('#view_personal_drafts').on('show.bs.modal', function(e) {
