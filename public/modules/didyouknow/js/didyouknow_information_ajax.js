e0e00c1d7836c742b9b73bade688a0623b65d704
kuijpers <darkknightworks@gmail.com>
2018-12-08 22:51:14 +0100

    Added request and minnor changes

--- public/modules/didyouknow/js/didyouknow_information_ajax.js
+++ public/modules/didyouknow/js/didyouknow_information_ajax.js
@@ -1,6 +1,8 @@
 	$(document).ready(function() {
 
-		$('#submit_new').on('click',function() {
+		$('#submit_new').on('click',function(e) {
+
+			e.preventDefault();
 
 			// input field name=title #=title
 
@@ -20,6 +22,57 @@
 
 		});
 
+		$('#edit_personal_drafts_submit').on('click',function(e){
+
+			e.preventDefault();
+
+			var url			=	"/api/board/information/update";
+
+			var id_field	=	$('#edit_personal_drafts_id');
+
+			var title_field	=	$('#edit_personal_drafts_title');
+
+			var body_field	=	$('#edit_personal_drafts_body');
+
+			var author_approved = $("#author_approved").prop("checked") ? 1 : 0;
+
+			var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+
+			$.ajax({
+				url: url,
+				method: 'post',
+				data: {
+					_token: CSRF_TOKEN,
+					type: 'edit_personal_drafts',
+					id: id_field.val(),
+					title: title_field.val(),
+					body: body_field.val(),
+					author_approved:author_approved
+				},
+				dataType: 'JSON',
+				complete: function(data) {
+					var Resp = data.responseText;
+					//console.log(Resp);
+				},
+				success: function(data){
+					console.log(data);
+
+					$.each(data.errors, function(key, value){
+						$('#modal_update_fail').show();
+						// @todo: Alerts don't show up
+						$('#modal_update_fail_alert').append('<p class="remove-alert">'+value+'</p>');
+						$('#modal_update_fail').delay( 5000 ).fadeOut( 'slow' );
+					});
+
+					$.each(data.success, function(key, value){
+						$('#modal_update_success').show();
+						$('#modal_update_success_alert').append('<p class="remove-alert">'+value+'</p>');
+						$('#modal_update_success').delay( 5000 ).fadeOut( 'slow' );
+					});
+				}});
+			$( ".remove-alert" ).remove();
+		});
 
 
 		function create_new_article(title , body , author_approved , editor_approved){
