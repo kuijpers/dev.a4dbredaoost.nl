4809ddfe6d664d2858be3d1c28314d68a6b67b9c
kuijpers <darkknightworks@gmail.com>
2018-12-09 23:15:16 +0100

    Added new assets for ajax calls

--- Modules/Didyouknow/Resources/views/board/segments/information_ajaxCalls.blade.php
+++ Modules/Didyouknow/Resources/views/board/segments/information_ajaxCalls.blade.php
@@ -1,1 +1,24 @@
 <script src="{{asset("/modules/didyouknow/js/didyouknow_information_ajax.js")}}"></script>
+
+{{--
+        * 	Set-up the data required to display in the modals
+        * 	Data needs to be send as a JSON string via buttons
+--}}
+
+{{--Personal Drafts--}}
+<script src="{{ Module::asset('didyouknow:js/ajaxCalls/personal_drafts.js') }}"></script>
+
+{{--Editor--}}
+<script src="{{ Module::asset('didyouknow:js/ajaxCalls/author_approved.js') }}"></script>
+
+{{--Publisher--}}
+<script src="{{ Module::asset('didyouknow:js/ajaxCalls/editor_approved.js') }}"></script>
+
+{{--Published--}}
+<script src="{{ Module::asset('didyouknow:js/ajaxCalls/publisher_approved.js') }}"></script>
+
+{{--Archived--}}
+<script src="{{ Module::asset('didyouknow:js/ajaxCalls/archived.js') }}"></script>
+
+{{--New--}}
+<script src="{{ Module::asset('didyouknow:js/ajaxCalls/new.js') }}"></script>
