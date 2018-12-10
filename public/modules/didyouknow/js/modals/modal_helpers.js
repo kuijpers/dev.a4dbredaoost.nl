963e1e853b6cfea20fc751d7abbbe58cbcb75413
kuijpers <darkknightworks@gmail.com>
2018-12-10 22:18:57 +0100

    Open active tab

--- public/modules/didyouknow/js/modals/modal_helpers.js
+++ public/modules/didyouknow/js/modals/modal_helpers.js
@@ -191,3 +191,20 @@ function checkbox_set_show_timepickers_div(){
 	});
 
 }
+
+// Get back to TAB after page refresh or visiting other pages
+
+$('a[data-toggle="tab"]').click(function (e) {
+	e.preventDefault();
+	$(this).tab('show');
+});
+
+$('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
+	var id = $(e.target).attr("href");
+	localStorage.setItem('selectedTab', id)
+});
+
+var selectedTab = localStorage.getItem('selectedTab');
+if (selectedTab != null) {
+	$('a[data-toggle="tab"][href="' + selectedTab + '"]').tab('show');
+}
