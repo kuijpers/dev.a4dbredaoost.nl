d2f959f855b4c63b07e6b567fef5ced55e15bb75
kuijpers <darkknightworks@gmail.com>
2019-01-07 22:01:15 +0100

    DB need to be seeded

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,208 @@
+<?php
+
+use Illuminate\Database\Seeder;
+	use App\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		$roles = [
+
+			[
+				'name' 			=> 'Webmaster',
+				'slug' 			=> str_slug('webmaster', '-'),
+				'permissions' 	=> [
+									'set-webmaster'				=> true,
+									'set-roles-with-admin'		=> true,
+
+									'create-users'				=> true,
+									'ban-users'					=> true,
+									'blacklist-users'			=> true,
+									'delete-users'				=> true,
+									'view-delete-users'			=> true,
+									'destroy-users'				=> true,
+
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'blacklist-volunteers'		=> true,
+									'delete-volunteers'			=> true,
+									'view-delete-volunteers'	=> true,
+									'destroy-volunteers'		=> true,
+
+									'create-news-post' 			=> true,
+									'edit-news-post' 			=> true,
+									'publish-news-post' 		=> true,
+									'delete-news-post' 			=> true,
+									'view-deleted-news-posts' 	=> true,
+									'destroy-news-post' 		=> true,
+
+									'create-dyk-post' 			=> true,
+									'edit-dyk-post' 			=> true,
+									'publish-dyk-post' 			=> true,
+									'delete-dyk-post' 			=> true,
+									'view-deleted-dyk-posts' 	=> true,
+									'destroy-dyk-post' 			=> true,
+									'change-dyk-setting' 		=> true,
+
+									'create-dyki-post' 			=> true,
+									'edit-dyki-post' 			=> true,
+									'publish-dyki-post' 		=> true,
+									'delete-dyki-post' 			=> true,
+									'view-deleted-dyki-posts' 	=> true,
+									'destroy-dyki-post' 		=> true,
+									'change-dyki-setting' 		=> true,
+								],
+			],
+
+			[
+				'name' 			=> 'Admin',
+				'slug' 			=> str_slug('admin', '-'),
+				'permissions' 	=> [
+									'set-webmaster'				=> false,
+									'set-roles-without-admin'	=> true,
+
+									'create-users'				=> true,
+									'ban-users'					=> true,
+									'delete-users'				=> true,
+
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'delete-volunteers'			=> true,
+
+									'create-news-post' 			=> true,
+									'edit-news-post' 			=> true,
+									'publish-news-post' 		=> true,
+									'delete-news-post' 			=> true,
+									'view-deleted-news-posts' 	=> true,
+									'destroy-news-post' 		=> true,
+
+									'create-dyk-post' 			=> true,
+									'edit-dyk-post' 			=> true,
+									'publish-dyk-post' 			=> true,
+									'delete-dyk-post' 			=> true,
+									'view-deleted-dyk-posts' 	=> true,
+									'destroy-dyk-post' 			=> true,
+									'change-dyk-setting' 		=> true,
+
+									'create-dyki-post' 			=> true,
+									'edit-dyki-post' 			=> true,
+									'publish-dyki-post' 		=> true,
+									'delete-dyki-post' 			=> true,
+									'view-deleted-dyki-posts' 	=> true,
+									'destroy-dyki-post' 		=> true,
+									'change-dyki-setting' 		=> true,
+				],
+			],
+
+			[
+				'name' 			=> 'Author',
+				'slug' 			=> str_slug('author', '-'),
+				'permissions' 	=> [
+									'create-news-post' 			=> true,
+									'edit-own-news-post' 		=> true,
+									'delete-own-news-post' 		=> true,
+
+									'create-dyk-post' 			=> true,
+									'edit-own-dyk-post' 		=> true,
+									'delete-own-dyk-post' 		=> true,
+
+									'create-dyki-post' 			=> true,
+									'edit-own-dyki-post' 		=> true,
+									'delete-own-dyki-post' 		=> true,
+								],
+			],
+
+			[
+				'name' 			=> 'Editor',
+				'slug' 			=> str_slug('editor', '-'),
+				'permissions' 	=> [
+									'edit-news-posts' 			=> true,
+									'delete-news-post' 			=> true,
+
+									'edit-dyk-posts' 			=> true,
+									'delete-dyk-post' 			=> true,
+
+									'edit-dyki-posts' 			=> true,
+									'delete-dyki-post' 			=> true,
+								],
+			],
+
+			[
+				'name' 			=> 'Publisher',
+				'slug' 			=> str_slug('publisher', '-'),
+				'permissions' 	=> [
+									'publish-news-post' 		=> true,
+									'delete-news-post' 			=> true,
+
+									'publish-dyk-post' 			=> true,
+									'delete-dyk-post' 			=> true,
+
+									'publish-dyki-post' 		=> true,
+									'delete-dyki-post' 			=> true,
+								],
+			],
+
+
+
+			[
+				'name' 			=> 'President',
+				'slug' 			=> str_slug('president', '-'),
+				'permissions' 	=> [
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'delete-volunteers'			=> true,
+									],
+			],
+
+			[
+				'name' 			=> 'Vice President',
+				'slug' 			=> str_slug('vice president', '-'),
+				'permissions' 	=> [
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'delete-volunteers'			=> true,
+									],
+			],
+
+			[
+				'name' 			=> 'Secretary',
+				'slug' 			=> str_slug('secretary', '-'),
+				'permissions' 	=> [
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'delete-volunteers'			=> true,],
+									],
+
+			[
+				'name' 			=> 'Treasurer',
+				'slug' 			=> str_slug('treasurer', '-'),
+				'permissions' 	=> [
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'delete-volunteers'			=> true,
+									],
+			],
+
+			[
+				'name' 			=> 'Member',
+				'slug' 			=> str_slug('member', '-'),
+				'permissions' 	=> [
+									'create-volunteers'			=> true,
+									'ban-volunteers'			=> true,
+									'delete-volunteers'			=> true,
+									],
+			],
+
+
+		];
+		foreach ($roles as $key => $value) {
+			Role::create($value);
+		}
+    }
+}
