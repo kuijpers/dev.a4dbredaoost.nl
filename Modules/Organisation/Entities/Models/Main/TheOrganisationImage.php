4d5b97f4b66d1213dc098b2bca07ab81196b61fc
kuijpers <darkknightworks@gmail.com>
2019-01-23 22:49:52 +0100

    Initial Models

---
+++ Modules/Organisation/Entities/Models/Main/TheOrganisationImage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Organisation\Entities\Models\Main;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TheOrganisationImage extends Model
+{
+    protected $fillable = [];
+}
