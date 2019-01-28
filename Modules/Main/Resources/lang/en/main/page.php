7d79d49b4dcd0087b2bf33d4b91a479ac196ba52
kuijpers <darkknightworks@gmail.com>
2019-01-28 14:56:21 +0100

    Initial lang files

---
+++ Modules/Main/Resources/lang/en/main/page.php
@@ -0,0 +1,24 @@
+<?php
+
+	return [
+
+		// Titles
+		'title_spotlight' 					=> 'In the spotlight',
+		'title_latest_news' 				=> 'Latest news',
+		'title_whats_happening' 			=> 'What\'s going to happen',
+		'title_didyouknow' 					=> 'Did you know ?',
+		'title_volunteers' 					=> 'Volunteers',
+		'title_participants' 				=> 'Participants',
+		'title_sponsors' 					=> 'Sponsors',
+
+		// Buttons
+		'button_more_info' 					=> 'More information',
+		'button_news_message' 				=> 'Go to this news message',
+		'button_more_dates' 				=> 'More dates',
+		'button_more_didyouknow' 			=> 'More did you knows',
+
+
+		// Errors
+		'message_no_data' 					=> 'No data found.',
+
+	];
---
+++ Modules/Main/Resources/lang/nl/main/page.php
@@ -0,0 +1,24 @@
+<?php
+
+	return [
+
+		// Titles
+		'title_spotlight' 					=> 'In het zonnetje',
+		'title_latest_news' 				=> 'Het laatste nieuws',
+		'title_whats_happening' 			=> 'Wat gaat er gebeuren',
+		'title_didyouknow' 					=> 'Wist u dat?',
+		'title_volunteers' 					=> 'Vrijwilligers',
+		'title_participants' 				=> 'Deelnemers',
+		'title_sponsors' 					=> 'Sponsors',
+
+		// Buttons
+		'button_more_info' 					=> 'Meer informatie',
+		'button_news_message' 				=> 'Naar dit nieuwsbericht',
+		'button_more_dates' 				=> 'Meer datums',
+		'button_more_didyouknow' 			=> 'Meer wist u datjes',
+
+
+		// Errors
+		'message_no_data' 					=> 'Geen gegevens gevonden.',
+
+	];
