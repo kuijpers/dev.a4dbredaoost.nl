c4cb29391ad4efaff94b608b15aaef7b72ab1e37
kuijpers <darkknightworks@gmail.com>
2019-02-03 00:04:50 +0100

    Updated routes file because file was moved

--- Modules/Main/Http/routes.php
+++ Modules/Main/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'namespace' => 'Modules\Main\Http\Controllers'], function()
+Route::group(['middleware' => 'web', 'namespace' => 'Modules\Main\Http\Controllers\Main'], function()
 {
     Route::get('/', 'MainController@index')->name('main.main.index');
 });
