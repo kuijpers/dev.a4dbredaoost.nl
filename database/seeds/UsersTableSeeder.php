01bab7734b601e7b33ec56e4bbd64b5215b29570
kuijpers <darkknightworks@gmail.com>
2019-02-03 00:06:12 +0100

    Added data to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
+	use App\Models\User;
+	use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,14 +12,55 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name'        	=> 'firstname lastname',
-			'slug' 			=> str_slug('firstname lastname'.time(), '-'),
-			'email'         => 'user@test.nl',
-			'password'      => bcrypt('welkom'),
-			'created_at'	=> date('Y-m-d H:i:s'),
-			'updated_at'    => date('Y-m-d H:i:s'),
-		]);
+//        DB::table('users')->insert([
+//            'name'        	=> 'firstname lastname',
+//			'slug' 			=> str_slug('firstname lastname'.time(), '-'),
+//			'email'         => 'user@test.nl',
+//			'password'      => bcrypt('welkom'),
+//			'created_at'	=> date('Y-m-d H:i:s'),
+//			'updated_at'    => date('Y-m-d H:i:s'),
+//		]);
+
+
+		$users = [
+			[
+				'name'        	=> 'firstname1 lastname1',
+				'slug' 			=> str_slug('firstname1 lastname1-'.time(), '-'),
+				'email'         => 'user1@test.nl',
+				'password'      => bcrypt('welkom'),
+				'created_at'	=> date('Y-m-d H:i:s'),
+				'updated_at'    => date('Y-m-d H:i:s'),
+			],
+			[
+				'name'        	=> 'firstname2 lastname2',
+				'slug' 			=> str_slug('firstname2 lastname2-'.time(), '-'),
+				'email'         => 'user2@test.nl',
+				'password'      => bcrypt('welkom'),
+				'created_at'	=> date('Y-m-d H:i:s'),
+				'updated_at'    => date('Y-m-d H:i:s'),
+			],
+			[
+				'name'        	=> 'firstname3 lastname3',
+				'slug' 			=> str_slug('firstname3 lastname3-'.time(), '-'),
+				'email'         => 'user3@test.nl',
+				'password'      => bcrypt('welkom'),
+				'created_at'	=> date('Y-m-d H:i:s'),
+				'updated_at'    => date('Y-m-d H:i:s'),
+			],
+			[
+				'name'        	=> 'firstname4 lastname4',
+				'slug' 			=> str_slug('firstname4 lastname4-'.time(), '-'),
+				'email'         => 'user4@test.nl',
+				'password'      => bcrypt('welkom'),
+				'created_at'	=> date('Y-m-d H:i:s'),
+				'updated_at'    => date('Y-m-d H:i:s'),
+			],
+
+
+		];
+		foreach ($users as $key => $value) {
+			User::create($value);
+		}
 
     }
 }
