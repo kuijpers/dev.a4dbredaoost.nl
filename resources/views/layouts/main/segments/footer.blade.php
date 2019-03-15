b7968c78224280545f6a184c51a3af4f7c8661d4
kuijpers <darkknightworks@gmail.com>
2019-03-15 11:34:51 +0100

    Built up footer section

--- resources/views/layouts/main/segments/footer.blade.php
+++ resources/views/layouts/main/segments/footer.blade.php
@@ -1,31 +1,66 @@
-<div class="row footer-contact bg-success p-3">
+{{--{{$curr_year}}--}}
+<div id="footer-address" class="row footer-contact bg-success p-3">
+
     <div class="col-md-4 offset-md-1">
         <address>
             <strong>
                 <div class="text-white">
-                    Example Inc.
+                    {{$organisation->value_text}}
                 </div>
             </strong>
             <br>
-            1234 Example Street
+            <strong>
+                <u>
+                    Secretariaat
+                </u>
+            </strong>
             <br>
-            Antartica, Example 0987
             <br>
-            <abbr title="Phone">P:</abbr> (123) 456-7890
+            <strong>{{$secretary->name}}</strong>
+            <br>
+            @if($secretary->show_address)
+                @isset($secretary->address)
+                    {{$secretary->address}}
+                    <br>
+                @endisset
+                @isset($secretary->postcode)
+                    {{$secretary->postcode}}
+                    @isset($secretary->city)
+                        ,
+                    @endisset
+                @endisset
+                @isset($secretary->city)
+                    {{$secretary->city}}
+                    <br>
+                @endisset
+            @endif
+            @if($secretary->show_phone)
+                @isset($secretary->phone)
+                    <abbr title="Telefoon">T:</abbr> {{$secretary->phone}}
+                    <br>
+                @endisset
+            @endif
+            @if($secretary->show_phone)
+                @isset($secretary->phone)
+                    <abbr title="Telefoon">T:</abbr> {{$secretary->phone}}
+                    <br>
+                @endisset
+            @endif
         </address>
 
         <address>
-            <strong>Full Name</strong><br>
-            <a href="mailto:#">exam.ple@example.com</a>
+            <a href="mailto:{{$secretary->email}}">{{$secretary->email}}</a>
         </address>
     </div>
+
+
 </div>
 
-<div class="row footer-copyright bg-success p-3">
+<div id="footer-copyright" class="row footer-copyright bg-success p-3">
     <div class="col-md-8 offset-md-2">
         De website is beschermd door copyright.
         <br>
-        &copy; 2018 A4D Breda-oost
+        &copy; {{$founded}} - {{$curr_year}} {{$organisation->value_text}}
         <br>
         <br>
         <a href="{{ route('main.credits.index') }}" class="text-white">Credits</a>
