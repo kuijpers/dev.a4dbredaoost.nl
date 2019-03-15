bb4da4e863b0041beca54da715b7eda52e7157f7
kuijpers <darkknightworks@gmail.com>
2019-03-15 11:35:11 +0100

    Set up service provider for footer section

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Main\WebSettings;
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\Schema;
 
 use App\Models\BoardmembersDashboardSettings;
+use Modules\Board\Http\Controllers\Main\TheBoardController;
 use Modules\SocialMedia\Entities\Models\Main\SocialMedia;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +29,16 @@ class AppServiceProvider extends ServiceProvider
 
         view()->composer('socialmedia::includes.main.social_media_component', function($view){
 			$view->with('social_media_items',SocialMedia::get_social_media());
+		});
+
+        view()->composer('layouts.main.segments.footer', function($view){
+
+        			$secretary = TheBoardController::get_secretary();
+        			$organisation = WebSettings::where('slug', '=', 'organisatie-naam')->first();
+        			$founded = WebSettings::where('slug', '=', 'oprichting')->pluck('value_text')->first();
+        			$curr_year = Carbon::now()->year;
+
+			$view->with(compact('secretary', 'organisation', 'founded', 'curr_year'));
 		});
 
     }
