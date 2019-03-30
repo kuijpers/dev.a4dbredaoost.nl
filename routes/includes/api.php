d7a49ffcf35f6eb2c561208dd0c6ea6727fcda77
kuijpers <darkknightworks@gmail.com>
2019-03-30 15:36:18 +0100

    Added routes

--- routes/includes/api.php
+++ routes/includes/api.php
@@ -4,8 +4,15 @@
 
 	Route::prefix('api')->group(function(){
 
+
+
+
+	// Board api routes
+		//
 		Route::post('/board/member/settings/theme-select', 'Board\MemberSettingsController@post_theme');
 
 		Route::post('/board/member/settings/language-select', 'Board\MemberSettingsController@post_language');
+
+		Route::post('/board/member/settings/show-web', 'Board\MemberSettingsController@update_contact_show_web_settings');
 
 	});
--- routes/includes/board.php
+++ routes/includes/board.php
@@ -15,6 +15,10 @@
 
 		Route::get('/password/reset/{token}', 'Auth\BoardmemberResetPasswordController@showResetForm')->name('board.password.reset');
 
+	// Changes made by user
+		Route::post('/password/change', 'Board\MemberProfileController@change_password')->name('board.password.change');
+
+
 //  Page routes
 		Route::get('/', 'Board\HomeController@index')->name('board.home');
 
@@ -24,6 +28,12 @@
 		Route::get('/lid', 'Board\MemberProfileController@index')->name('board.member');
 
 		Route::get('/lid/profiel', 'Board\MemberProfileController@index')->name('board.member.profile');
+
+		Route::post('/lid/profiel/update', 'Board\MemberProfileController@update_profile')->name('board.member.profile.update');
+
+		Route::post('/lid/aboutme/update', 'Board\MemberProfileController@update_about_me')->name('board.member.aboutme.update');
+
+		Route::post('/lid/avatar/update', 'Board\MemberProfileController@update_avatar')->name('board.member.avatar.update');
 
 		Route::get('/lid/instellingen', 'Board\MemberSettingsController@index')->name('board.member.settings');
 
